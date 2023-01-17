5bf52ea7627e4ee16241cc55e1e8912584ed999f
Amit Kumar Thakur <kingston.amit9830@gmail.com>
2023-01-17 20:48:11 +0530

    router updated

--- router/route.js
+++ router/route.js
@@ -6,45 +6,27 @@ const router = Router();
 import * as controller from '../controllers/appController.js';
 import auth, { localVaribales } from '../middleware/auth.js'
 import { registerMail } from '../controllers/mailer.js'
+
+
 // POST methods*******************
+router.post('/register', controller.register);// for register the user
+router.post('/registerMail', registerMail);// for send the email
+router.post('/authenticate', controller.verifyUser, (req, res) => { res.end(); });// authenticate user
+router.post('/login', controller.verifyUser, controller.login);// login in app
 
-// for register the user
-router.post('/register', controller.register);
 
-// for send the email
-router.post('/registerMail', registerMail);
-
-// authenticate user
-router.post('/authenticate', (req, res) => {
-    res.end();
-});
-
-// login in app
-router.post('/login', controller.verifyUser, controller.login);
 
 // GET Methods*************
-
-// user with username
-router.get('/user/:username', controller.getUser);
-
-// generate random otp
-router.get('/generateOTP', controller.verifyUser, localVaribales, controller.generateOTP);
-
-// verify generated OTP
-router.get('/verifyOTP', controller.verifyOTP);
-
-// reset all the variables
-router.get('/createResetSession', controller.createResetSession);
+router.get('/user/:username', controller.getUser);// user with username
+router.get('/generateOTP', controller.verifyUser, localVaribales, controller.generateOTP);// generate random otp
+router.get('/verifyOTP', controller.verifyUser,controller.verifyOTP);// verify generated OTP
+router.get('/createResetSession', controller.createResetSession);// reset all the variables
 
 
 
 // PUT Methods*******************
-
-// to update the user profile
-router.put('/updateuser', auth, controller.updateUser);
-
-// to use for reset the password
-router.put('/resetPassword', controller.verifyUser, controller.resetPassword);
+router.put('/updateuser', auth, controller.updateUser);// to update the user profile
+router.put('/resetPassword', controller.verifyUser, controller.resetPassword);// to use for reset the password
 
 
 
